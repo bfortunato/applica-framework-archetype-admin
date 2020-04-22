c8001ef2db213e2089d3ae08aafaeac091bfb6ad
bimbobruno <bimbobruno@gmail.com>
2020-04-22 11:24:18 +0200

    dashboard

--- src/web/screens/home.jsx
+++ src/web/screens/home.jsx
@@ -1,73 +1,91 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 const { Screen, Layout } = require("../components/layout")
 const Secure = require("../components/secure")
+import Services from "../../api/genericApi"
+import Colors from "../../model/colors";
+import { optional } from "../../utils/lang";
 
 const Widgets = (props) => {
+    const [counters, setCounters] = useState({});
+
     useEffect(() => {
-        const me = ReactDOM.findDOMNode(this);
 
-        $('.sparkline-bar-stats').sparkline('html', {
-            type: 'bar',
-            height: 36,
-            barWidth: 3,
-            barColor: '#fff',
-            barSpacing: 2
-        });
+        Services.get("dashboard/data")
+            .then((response) => {
+                const me = ReactDOM.findDOMNode(this);
+                const counters = response.value;
 
-        // Make some sample data
-    var pieData = [
-        {data: 1, color: '#ff6b68', label: 'Toyota'},
-        {data: 2, color: '#03A9F4', label: 'Nissan'},
-        {data: 3, color: '#32c787', label: 'Hyundai'},
-        {data: 4, color: '#f5c942', label: 'Scion'},
-        {data: 5, color: '#d066e2', label: 'Daihatsu'}
-    ];
-    
-    // Pie Chart
-    if($('.flot-pie')[0]){
-        $.plot('.flot-pie', pieData, {
-            series: {
-                pie: {
-                    show: true,
-                    stroke: {
-                        width: 2
-                    }
+                $('.sparkline-bar-stats').sparkline('html', {
+                    type: 'bar',
+                    height: 36,
+                    barWidth: 3,
+                    barColor: '#fff',
+                    barSpacing: 2
+                });
+
+                var testsData = [
+                    {data: counters.positiveTests, color: Colors.error, label: "Positivi"},
+                    {data: counters.negativeTests, color: Colors.success, label: "Negativi"},
+                    {data: counters.unknownTests, color: Colors.warning, label: "Non effettuati"},
+                ];
+
+                var profilesData = [
+                    {data: counters.familyMembers, color: "#03A9F4", label: "Familiari"},
+                    {data: counters.parents, color: "#f5c942", label: "Utenti"},
+                ];
+                
+                // Pie Chart
+                if($('.flot-pie')[0]){
+                    $.plot('.flot-pie', testsData, {
+                        series: {
+                            pie: {
+                                show: true,
+                                stroke: {
+                                    width: 2
+                                }
+                            }
+                        },
+                        legend: {
+                            container: '.flot-chart-legend--pie',
+                            backgroundOpacity: 0.5,
+                            noColumns: 0,
+                            backgroundColor: "white",
+                            lineWidth: 0,
+                            labelBoxBorderColor: '#fff'
+                        }
+                    });
                 }
-            },
-            legend: {
-                container: '.flot-chart-legend--pie',
-                backgroundOpacity: 0.5,
-                noColumns: 0,
-                backgroundColor: "white",
-                lineWidth: 0,
-                labelBoxBorderColor: '#fff'
-            }
-        });
-    }
-    
-    // Donut Chart
-    if($('.flot-donut')[0]){
-        $.plot('.flot-donut', pieData, {
-            series: {
-                pie: {
-                    innerRadius: 0.5,
-                    show: true,
-                    stroke: { 
-                        width: 2
-                    }
+                
+                // Donut Chart
+                if($('.flot-donut')[0]){
+                    $.plot('.flot-donut', profilesData, {
+                        series: {
+                            pie: {
+                                innerRadius: 0.5,
+                                show: true,
+                                stroke: { 
+                                    width: 2
+                                }
+                            }
+                        },
+                        legend: {
+                            container: '.flot-chart-legend--donut',
+                            backgroundOpacity: 0.5,
+                            noColumns: 0,
+                            backgroundColor: "white",
+                            lineWidth: 0,
+                            labelBoxBorderColor: '#fff'
+                        }
+                    });
                 }
-            },
-            legend: {
-                container: '.flot-chart-legend--donut',
-                backgroundOpacity: 0.5,
-                noColumns: 0,
-                backgroundColor: "white",
-                lineWidth: 0,
-                labelBoxBorderColor: '#fff'
-            }
-        });
-    }
+
+                setCounters(counters);
+            })
+            .catch(e => console.error(e));
+
+
+    
 
     }, []);
 
@@ -95,7 +113,7 @@ const Widgets = (props) => {
             <div className="col-sm-6 col-md-3">
                 <div className="quick-stats__item bg-blue">
                     <div className="quick-stats__info">
-                        <h2>1546</h2>
+                        <h2>{optional(counters.parents, 0)}</h2>
                         <small>Utenti registrati</small>
                     </div>
 
@@ -106,7 +124,7 @@ const Widgets = (props) => {
             <div className="col-sm-6 col-md-3">
                 <div className="quick-stats__item bg-amber">
                     <div className="quick-stats__info">
-                        <h2>2194</h2>
+                        <h2>{optional(counters.familyMembers, 0)}</h2>
                         <small>Familiari censiti</small>
                     </div>
 
@@ -117,7 +135,7 @@ const Widgets = (props) => {
             <div className="col-sm-6 col-md-3">
                 <div className="quick-stats__item bg-purple">
                     <div className="quick-stats__info">
-                        <h2>$58,778</h2>
+                        <h2>{optional(counters.contacts, 0)}</h2>
                         <small>Contatti registrati</small>
                     </div>
 
@@ -128,7 +146,7 @@ const Widgets = (props) => {
             <div className="col-sm-6 col-md-3">
                 <div className="quick-stats__item bg-red">
                     <div className="quick-stats__info">
-                        <h2>214</h2>
+                        <h2>{optional(counters.positiveTests, 0)}</h2>
                         <small>Tamponi positivi</small>
                     </div>
 
@@ -141,7 +159,7 @@ const Widgets = (props) => {
             <div className="col-sm-6">
                 <div className="card">
                     <div className="card-body">
-                        <h4 className="card-title">Grafico utenti</h4>
+                        <h4 className="card-title">Grafico tamponi</h4>
 
                         <div className="flot-chart flot-pie"></div>
                         <div className="flot-chart-legends flot-chart-legend--pie"></div>
@@ -152,7 +170,7 @@ const Widgets = (props) => {
             <div className="col-sm-6">
                 <div className="card">
                     <div className="card-body">
-                        <h4 className="card-title">Grafico tamponi</h4>
+                        <h4 className="card-title">Grafico utenti</h4>
 
                         <div className="flot-chart flot-donut"></div>
                         <div className="flot-chart-legends flot-chart-legend--donut"></div>
